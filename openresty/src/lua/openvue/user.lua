1668d98ef4e1e7470a69111e8626306d806ba88a
yingxuepan <yingxuepan@sohu-inc.com>
2021-06-28 15:23:08 +0800

    登录

--- openresty/src/lua/openvue/user.lua
+++ openresty/src/lua/openvue/user.lua
@@ -7,9 +7,42 @@
 
 local cjson = require "cjson"
 local xxtea = require "xxtea"
+local str = require "utils.str"
 local db = require "utils.mysql"
 local conf = require "openvue.conf"
 local lang = require "openvue.lang"
+
+local function checkUser(context)
+    local user = nil
+
+    if context.req and context.req.args and context.req.args.token then
+        local token = xxtea.decrypt(context.req.args.token, 'Adp201609203059Z')
+        local infos = str.split(token, ':')
+        if #infos >= 3 then
+            -- 先不校验过期时间
+            local sql = string.format("SELECT b.id, a.open_code, b.name, b.head_img_url, b.mobile, c.code   FROM account a, user b, user_group c, user_group_user d WHERE a.user_id = b.id AND b.id = d.user_id AND c.id = d.user_group_id AND a.deleted = 0 AND b.deleted = 0 AND c.deleted = 0 AND d.deleted = 0 AND a.open_code = '%s' and b.password = '%s'", infos[2], infos[3])
+            local users = db.query(conf.db_option, sql, 10)
+            if users then
+                local user_map = {}
+                for i, v in ipairs(users) do
+                    if user_map[infos[2]] then
+                        table.insert(user_map[infos[2]].roles, v.code)
+                    else
+                        user_map[infos[2]] = {}
+                        user_map[infos[2]].name = v.name
+                        user_map[infos[2]].introduction = ''
+                        user_map[infos[2]].avatar = v.head_img_url
+                        user_map[infos[2]].roles = {}
+                        table.insert(user_map[infos[2]].roles, v.code)
+                    end
+                end
+                user = user_map[infos[2]]
+            end
+        end
+    end
+
+    return user
+end
 
 local function handleLogin(context)
     local data = {}
@@ -18,7 +51,7 @@ local function handleLogin(context)
     local passwords = db.query(conf.db_option, sql, 10)
     if passwords and passwords[1] and passwords[1].password == context.params.password then
         local val = string.format("%d:%s:%s", ngx.now(), context.params.username, context.params.password)
-        local token = xxtea.encrypt(val, "Adp201609203059Z")
+        local token = xxtea.encrypt(val, 'Adp201609203059Z')
         data.code = 20000
         data.data = {}
         data.data.token = token
@@ -33,12 +66,14 @@ end
 local function handleInfo(context)
     local data = {}
 
-    data.code = 20000
-    data.data = {}
-    data.data.roles = {"admin"}
-    data.data.introduction = "I am a super administrator"
-    data.data.avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
-    data.data.name = "Admin"
+    local user = checkUser(context)
+    if user then
+        data.code = 20000
+        data.data = user
+    else
+        data.code = 60204
+        data.message = 'Account and password are incorrect'
+    end
 
     return data
 end
